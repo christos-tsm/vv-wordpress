abe5d5fd248c82d1de1774bf044f64899e98941f
Christos TSM <christosgsd@gmail.com>
2023-05-17 22:50:08 +0300

    refacor single content of cpt and added the_content

--- template-parts/single-cpt/content.php
+++ template-parts/single-cpt/content.php
@@ -1,4 +1,5 @@
 <?php
+$post_type = get_post_type();
 $cover_photo = get_field('cover_photo');
 $gallery = get_field('gallery');
 $address = get_field('address');
@@ -14,42 +15,53 @@ $avg_prce = get_field('average_price');
 $categories = get_post_taxonomies(get_the_ID());
 ?>
 <main class="site-main site-main--single">
-    <div class="cover-photo__container container container--medium">
-        <?php if ($cover_photo && !empty($cover_photo)) : ?>
-            <img class="cover-photo" src="<?= $cover_photo['url'] ?>" alt="Volos-Voyage - <?php the_title(); ?>">
-        <?php else : ?>
-            <?php $cover_photo = get_field('header_logo', 'option'); ?>
-            <img class="cover-photo cover-photo--logo" src="<?= esc_url($cover_photo['url']) ?>" alt="Volos-Voyage - <?php the_title(); ?>">
-        <?php endif;  ?>
-        <?php if (has_post_thumbnail()) : ?>
-            <div class="profile-photo__container">
-                <img class="profile-photo" src="<?php the_post_thumbnail_url('large') ?>" alt="Volos-Voyage - <?php the_title(); ?>">
+    <article id="single-<?= get_the_ID(); ?>" class="single-content single-content__<?= esc_attr($post_type); ?>">
+        <header class="single-content__header">
+            <div class="cover-photo__container container container--medium">
+                <?php if ($cover_photo && !empty($cover_photo)) : ?>
+                    <img class="cover-photo" src="<?= $cover_photo['url'] ?>" alt="Volos-Voyage - <?php the_title(); ?>">
+                <?php else : ?>
+                    <?php $cover_photo = get_field('header_logo', 'option'); ?>
+                    <img class="cover-photo cover-photo--logo" src="<?= esc_url($cover_photo['url']) ?>" alt="Volos-Voyage - <?php the_title(); ?>">
+                <?php endif;  ?>
+                <?php if (has_post_thumbnail()) : ?>
+                    <div class="profile-photo__container">
+                        <img class="profile-photo" src="<?php the_post_thumbnail_url('large') ?>" alt="Volos-Voyage - <?php the_title(); ?>">
+                    </div>
+                <?php endif;  ?>
             </div>
-        <?php endif;  ?>
-    </div>
-    <section class="single-title__container container container--medium">
-        <h1 class="section-title section-title--single">
-            <?php the_title(); ?>
-            <?php foreach ($categories as $category) : ?>
-                <?php if (strpos($category, '-categories') !== false) : ?>
-                    <?php $terms = get_the_terms(get_the_ID(), $category); ?>
-                    <?php if ($terms && !is_wp_error($terms)) : ?>
-                        <?php foreach ($terms as $term) : ?>
-                            <span class="single-category"><?= $term->name; ?></span>
-                        <?php endforeach ?>
-                    <?php endif; ?>
-                <?php endif; ?>
-            <?php endforeach;  ?>
-        </h1>
-        <ul class="single-details">
-            <li class="single-details__item single-details__item--address">
-                <a href="#!" target="_blank" rel="noreferrer noopener" class="address-link">
-                    <span class="icon icon--x-small"><?= file_get_contents(get_stylesheet_directory() . '/assets/images/location-pin.svg') ?></span>
-                    <?= $address; ?>
-                </a>
-            </li>
-
-        </ul>
-
-    </section>
+            <div class="single-title__container container container--medium">
+                <h1 class="section-title section-title--single">
+                    <?php the_title(); ?>
+                    <?php foreach ($categories as $category) : ?>
+                        <?php if (strpos($category, '-categories') !== false) : ?>
+                            <?php $terms = get_the_terms(get_the_ID(), $category); ?>
+                            <?php if ($terms && !is_wp_error($terms)) : ?>
+                                <?php foreach ($terms as $term) : ?>
+                                    <span class="single-category"><?= $term->name; ?></span>
+                                <?php endforeach ?>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    <?php endforeach;  ?>
+                </h1>
+                <ul class="single-details">
+                    <li class="single-details__item single-details__item--address">
+                        <a href="#!" target="_blank" rel="noreferrer noopener" class="address-link">
+                            <span class="icon icon--x-small"><?= file_get_contents(get_stylesheet_directory() . '/assets/images/location-pin.svg') ?></span>
+                            <?= $address; ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+        <section class="single-content__content container container--medium">
+            <div class="section-title__container">
+                <h2 class="section-title"><?php pll_e('Σχετικά με εμάς'); ?> </h2>
+            </div>
+            <?php the_content();  ?>
+        </section>
+        <footer class="single-content__footer">
+            <!-- Contact details -->
+        </footer>
+    </article>
 </main>
